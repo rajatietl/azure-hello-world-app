7b2cb4b10e8fdb53d38e611907538ca9d775a64c
Rajat Singhal <33342708+rajatietl@users.noreply.github.com>
2024-08-20 10:44:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,15 @@
 from flask import Flask
 import os
-import logging
 from opencensus.ext.azure.log_exporter import AzureLogHandler
 
 app = Flask(__name__)
 
-# Configure logging with timestamp
-logging.basicConfig(format='%(asctime)s - %(message)s', level=logging.INFO)
-logger = logging.getLogger(__name__)
-logger.addHandler(AzureLogHandler(connection_string=os.getenv("APPINSIGHTS_CONNECTION_STRING")))
+# Application Insights configuration
+app.logger.addHandler(AzureLogHandler(connection_string=os.getenv("APPINSIGHTS_CONNECTION_STRING")))
 
 @app.route('/')
 def hello_world():
-    logger.info("Hello World endpoint was accessed")
+    app.logger.info("Hello World endpoint was accessed")
     return "Hello, World!"
 
 if __name__ == '__main__':
